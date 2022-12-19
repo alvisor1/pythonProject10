13b7b2bdff5533482b6cecd421e4a7624fd09c15
alvisor <alvisor@yandex.ru>
2022-12-19 16:26:24 +0300

    Исправлено

--- Sorokopyt_12.py
+++ Sorokopyt_12.py
@@ -1,27 +1,29 @@
+def summa(a,b):    return a+b
+def vychet(a,b):    return a-b
+def proiz(a,b):    return a*b
+def chast(a,b):    return a/b
+def pow(a,b): return a**b
+def sqr(a): return a**0.5
+from math import factorial
+def fact(a): return factorial(a)
 while True:
-    finish = int(input('Для выхода напишите 0, для продолжения - любую цифру: '))
-    if finish == 0:
+    c = input('Укажите арифметический знак либо 0 для завершения: ')
+    if c=='0':
+        print('Вычисления окончены')
         break
-    a = float(input('1 число: '))
-    oper = input('Выберите операцию +-/*: ')
-    b = float(input('2 число: '))
-    if oper == '+':
-        def my_calc(a,b):
-            return a+b
-        print(my_calc(a,b))
-    elif oper == '-':
-        def my_calc(a,b):
-            return a-b
-        print(my_calc(a,b))
-    elif oper == '*':
-        def my_calc(a,b):
-            return a*b
-        print(my_calc(a,b))
-    elif oper == '/' and b != 0:
-        def my_calc(a,b):
-            return a/b
-        print(my_calc(a,b))
-    elif b == 0:
-        print('Нельзя делить на ноль!')
-    elif oper != '+' and oper != '-' and oper != '*' and oper != '/':
-        print('Введите правильное значение!')
+    a = float(input('Введите число: '))
+    if c=='√':
+        print(sqr(a))
+        continue
+    if c=='!':
+        a = int(a)
+        print(fact(a))
+        continue
+    b = float(input('Введите ещё одно число: '))
+    if c=='+':    print(summa(a,b))
+    if c=='-':    print(vychet(a,b))
+    if c=='*':    print(proiz(a,b))
+    if c=='/':
+        try:    print(chast(a,b))
+        except ZeroDivisionError:    print('На ноль делить нельзя!!!')
+    if c=='^': print(pow(a,b))
